c929e782b4332ee92d027385398c456445aea3a9
krantzinator <krantzrs8@gmail.com>
2017-07-28 19:57:51 -0400

    breaking apart components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
 
+import { TodoDataService } from './todo-data.service';
+import { Todo } from './todo';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  // Ask Angular DI system to inject the dependency
+  // associated with the dependency injection token `TodoDataService`
+  // and assign it to a property called `todoDataService`
+  constructor(private todoDataService: TodoDataService) {
+  }
+
+  onAddTodo(todo: Todo) {
+    this.todoDataService.addTodo(todo);
+  }
+
+  onToggleTodoComplete(todo) {
+    this.todoDataService.toggleTodoComplete(todo);
+  }
+
+  onRemoveTodo(todo) {
+    this.todoDataService.deleteTodoById(todo.id);
+  }
+
+  get todos() {
+    return this.todoDataService.getAllTodos();
+  }
+
 }
